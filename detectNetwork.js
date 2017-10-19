90a5ea441d5a4bde949d1d4e70d83002a6a991e5
James Jelenko <jamesjelenko@gmail.com>
2017-10-19 10:46:59 -0700

    Added Visa and MasterCard recognition.

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,20 +8,28 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
   var leadingNums = cardNumber.slice(0, 2)
   var amexPrefixes = ["34", "37"]
   var dinerPrefixes = ["38", "39"]
+  var visaPrefixes = ["4"]
+  var visaLengths = [13, 16, 19]
+  var masterPrefixes = ["51", "52", "53", "54", "55"]
 
   if (amexPrefixes.includes(leadingNums)){
   	if (cardNumber.length === 15) return "American Express"
   }
   if (dinerPrefixes.includes(leadingNums)){
   	if (cardNumber.length === 14) return "Diner's Club"
+  }
+  if (visaPrefixes.includes(leadingNums[0])){
+  	if (visaLengths.includes(cardNumber.length)) return "Visa"
+  }
+  if (masterPrefixes.includes(leadingNums)){
+  	if (cardNumber.length === 16) return "MasterCard"
   }
 };
 
